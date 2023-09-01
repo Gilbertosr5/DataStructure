d11cb0ca7b890b57237101e6713a72121a73b2aa
Gilberto Ribeiro <gillbertosr5@gmail.com>
2023-09-01 12:19:56 -0300

    HeapSort Added

--- aps.py
+++ aps.py
@@ -18,6 +18,30 @@ def bubbleSort(array):
         array[j+1] = key 
   print(f'BubbleSort: {array}')  
 
+def heapify(array, n, i):
+  maior = i 
+  esq = 2 * i + 1 
+  dir = 2 * i + 2 
+
+  if esq < n and array[i] < array[esq]:
+      maior = esq
+
+  if dir < n and array[maior] < array[dir]:
+      maior = dir
+
+  if maior != i:
+    (array[i], array[maior]) = (array[maior], array[i]) 
+    heapify(array, n, maior) 
+
+def heapSort(array):
+  n = len(array) 
+
+  for i in range(n // 2 - 1, -1, -1):
+    heapify(array, n, i)
+
+  for i in range(n - 1, 0, -1):
+    (array[i], array[0]) = (array[0], array[i]) 
+    heapify(array, i, 0) 
 
 
 ray = [9,3,6,5,1]
@@ -25,3 +49,4 @@ print(f"Array Before: {ray}")
 
 insertionSort(ray)
 bubbleSort(ray)
+heapSort(ray)
